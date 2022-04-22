0b721b3751070e3bd1eb8557cd89ec3699a8a95f
Phan Dinh Duan <duanpd01.bhsoft@gmail.com>
2022-04-22 17:31:45 +0700

    validate

--- main.js
+++ main.js
@@ -1,51 +1,64 @@
 function validateForm() {
-     const fname = document.getElementById("FName").value;
-     const lname = document.getElementById("LName").value;
-     const address = document.getElementById("Address").value;
-     const phone = document.getElementById("Phone").value;
-     const email = document.getElementById("exampleFormControlInput1").value;
-     const fname_error = document.getElementById("fname_error");
-     const lname_error = document.getElementById("lname_error");
-     const address_error = document.getElementById("address_error");
-     const phone_error = document.getElementById("phone_error");
-     const email_error = document.getElementById("email_error");
-     let pass = document.getElementById("Pass");
-     let pass_error = document.getElementById("pass_error");
-     let pass1 = pass.toString();
-    if(!fname){
-      fname_error.innerHTML= "Khong duoc de trong first name";
-    }
-    if(fname){
-      fname_error.innerHTML= "";
-    }
-    if(!lname){
-      lname_error.innerHTML= "Khong duoc de trong last name";
-    }
-    if(lname){
-      lname_error.innerHTML= "";
-    }
-    if(!address){
-      address_error.innerHTML="Khong duoc de trong address";
-    }
-    if(address){
-      fname_error.innerHTML="";
-    }
-    if(!phone){
-      phone_error.innerHTML="Khong duoc de trong Phone";
-    }
-    if(phone){
-      phone_error.innerHTML="";
-    }
-    if(!email){
-     email_error.innerHTML="Khong duoc de trong Phone";
-    if(email){
-      email_error.innerHTML="";
-    }
-    if(pass1.length < 8){
-      pass_error.innerHTML="Pass khong duoc nho hon 8 ky tu";
-    }else{
-      pass_error="";
-    }
+  const fname = document.getElementById("FName").value;
+  const lname = document.getElementById("LName").value;
+  const address = document.getElementById("Address").value;
+  const phone = document.getElementById("Phone").value;
+  const email = document.getElementById("exampleFormControlInput1").value;
+  const pass = document.getElementById("Pass").value;
+  const fname_error = document.getElementById("fname_error");
+  const lname_error = document.getElementById("lname_error");
+  const address_error = document.getElementById("address_error");
+  const phone_error = document.getElementById("phone_error");
+  const email_error = document.getElementById("email_error");
+  const pass_error = document.getElementById("pass_error");
+  const pass_er = document.getElementById("pass_err");
+  const input_error = document.getElementById("input_error");
+ if(!fname){
+   fname_error.innerHTML= "Khong duoc de trong first name";
+ }
+ if(fname){
+   fname_error.innerHTML= "";
+ }
+ if(!lname){
+   lname_error.innerHTML= "Khong duoc de trong last name";
+ }
+ if(lname){
+   lname_error.innerHTML= "";
+ }
+ if(!address){
+   address_error.innerHTML="Khong duoc de trong address";
+ }
+ if(address){
+   fname_error.innerHTML="";
+ }
+ if(!phone){
+   phone_error.innerHTML="Khong duoc de trong Phone";
+ }
+ if(phone){
+   phone_error.innerHTML="";
+ }
+ if(!email){
+  email_error.innerHTML="Khong duoc de trong email";
+ }
+ if(email){
+   email_error.innerHTML="";
+ }
+ if(!pass){
+   pass_error.innerHTML="Khong duoc de trong Password";
+  }
+ if(pass){
+    pass_error.innerHTML="";
+  }
+ if(pass.length < 8){
+   pass_error.innerHTML="Pass khong duoc nho hon 8 ky tu";
+ }else{
+   pass_error.innerHTML="";
+ }
+ if(fname.length>30||lname.length>30||phone.length>30||email.length>30||pass.length>30||address.length>30){
+   input_error.innerHTML="Truong nhap vao khong duoc lon hon 30 ky tu";
+ }else{
+input_error.innerHTML ="";
+ }
 }
 
 
@@ -105,4 +118,3 @@ function validateForm() {
 //   }else{
 //     console.log("Moi ban nhap lai thong tin");
 //   }
- 
